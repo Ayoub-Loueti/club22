0b7cafc306264fb5f2235bca4acb6dfca0bee3bb
Raniabenali1 <benalirania855@gmail.com>
2024-02-18 00:44:55 +0100

    login design

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,44 +1,176 @@
 import React, { useState } from 'react';
+import '../assets/login.css';
+import ooredoo1Image from '../assets/ooredoo1.png';
+import ooredoo3Image from '../assets/ooredoo3.png';
 
-const login = () => {
+function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
 
-  const handleLogin = () => {
-    // Validation du formulaire de connexion
-    if (username === 'utilisateur' && password === 'motdepasse') {
-      // Connexion réussie, effectuez ici votre logique de connexion
-      console.log('Connexion réussie');
-    } else {
-      // Afficher une erreur si les identifiants sont incorrects
-      setError("Nom d'utilisateur ou mot de passe incorrect");
-    }
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Add your login logic here
   };
 
   return (
-    <div>
-      <h2>Connexion</h2>
-      <div>
-        <label>Nom d'utilisateur:</label>
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
+    <div className="login-page">
+      <div className="white-square">
+        <img
+          src={ooredoo1Image}
+          alt="logo ooredoo"
+          style={{
+            width: '160px',
+            top: '10px',
+            left: '23px',
+            position: 'relative',
+          }}
+        />
+        <div className="gray-rectangle">
+          <div className="form-column">
+            <h2
+              style={{
+                position: 'absolute',
+                top: '1px',
+                left: '30px',
+                color: '#2B3467',
+                fontFamily: 'inherit',
+                fontWeight: '800',
+                fontSize: '40px',
+              }}
+            >
+              Log In
+            </h2>
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <h3 className="input-label">Utilisateur</h3>
+                <input
+                  type="text"
+                  value={username}
+                  onChange={handleUsernameChange}
+                  className="input-field"
+                />
+              </div>
+              <div className="form-group">
+                <h3 className="input-label">Mot de passe</h3>
+                <input
+                  type="password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                  className="input-field"
+                />
+              </div>
+
+              <div className="form-group">
+                <input
+                  type="checkbox"
+                  id="remember-me"
+                  style={{ marginRight: '10px' }}
+                />
+                <label htmlFor="remember-me" style={{ color: '#4F5475' }}>
+                  Rester connecté
+                </label>
+              </div>
+              <button
+                type="submit"
+                style={{
+                  backgroundColor: '#191F43',
+                  color: '#fff',
+                  padding: '14px 55px',
+                  border: 'none',
+                  borderRadius: '14px',
+                 boxShadow: '0px 4px 12px 4px rgba(43, 52, 103, 0.5)',
+                  fontSize: '16px',
+                  fontWeight: 'bold',
+                  cursor: 'pointer',
+                  position: 'absolute',
+                  top: '80%',
+                  left: '50%',
+                  transform: 'translate(-50%, -50%)',
+                  zIndex: '1',
+                }}
+              >
+                log in
+              </button>
+            </form>
+          </div>
+        </div>              <div style={{ textAlign: 'center', marginTop: '20px', color: '#4F5475' }}>OU</div>
+
+      </div>
+      <div className="green-square">
+        <h3
+          style={{
+            fontFamily: 'inherit',
+            fontSize: '37px',
+            lineHeight: '1.5',
+            fontWeight: '300px',
+            color: '#fff',
+            textAlign: 'center',
+            position: 'absolute',
+            top: '40%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            zIndex: '1',
+          }}
+        >
+          DECOUVREZ DES <br /> BONS PLANS
+        </h3>
+        <p
+          style={{
+            fontFamily: 'inherit',
+            fontSize: '17px',
+            lineHeight: '35.1px',
+            color: '#fff',
+            textAlign: 'center',
+            position: 'absolute',
+            top: '60%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            zIndex: '1',
+          }}
+        >
+          Vous n'avez pas de compte ?
+        </p>
+        <button
+          style={{
+            backgroundColor: '#C50F10',
+            color: '#fff',
+            padding: '14px 35px',
+            border: 'none',
+            borderRadius: '14px',
+            boxShadow: '0px 4px 12px 4px rgba(255, 255, 255, 0.5)',
+            fontSize: '16px',
+            fontWeight: 'bold',
+            cursor: 'pointer',
+            position: 'absolute',
+            top: '70%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            zIndex: '1',
+          }}
+        >
+          S'inscrire
+        </button>
+
+        <img
+          src={ooredoo3Image}
+          alt="background image"
+          style={{
+            width: '100%',
+            height: '100%',
+            objectFit: 'cover',
+          }}
         />
       </div>
-      <div>
-        <label>Mot de passe:</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <button onClick={handleLogin}>Se connecter</button>
-      {error && <div style={{ color: 'red' }}>{error}</div>}
     </div>
   );
-};
+}
 
-export default login;
+export default Login;
