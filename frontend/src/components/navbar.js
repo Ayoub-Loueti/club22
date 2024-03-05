8e79a67acd41f220a96a903969962b0c02458352
Raniabenali1 <benalirania855@gmail.com>
2024-03-05 17:19:15 +0100

    kk

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../assets/navbar.css';
 import logo from '../assets/ooredoo2.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -7,29 +7,41 @@ import {
   faUserGroup,
   faCalendar,
   faUser,
-
+  faBars,
 } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
-
+  const [isVisible, setIsVisible] = useState(false);
+  // Function to toggle visibility
+  const toggleNavbar = () => {
+    setIsVisible(!isVisible);
+  };
 
   return (
-  
-      <nav className="navbar">
+    <div>
+      {/* Toggle Button */}
+      <div className="navbar-toggle" onClick={toggleNavbar}>
+        <FontAwesomeIcon icon={faBars} />
+      </div>
+
+      {/* Navbar */}
+      <nav className={`navbar ${isVisible ? 'visible' : ''}`}>
         <img src={logo} alt="Logo" className="navbar-logo" />
         <div className="icon-containerrr">
           <FontAwesomeIcon icon={faHouse} className="navbar-iconnn" />
           <FontAwesomeIcon icon={faUserGroup} className="navbar-iconnn" />
           <FontAwesomeIcon icon={faCalendar} className="navbar-iconnn" />
         </div>
+
         <div className="navbar-inner">
           <FontAwesomeIcon icon={faUser} className="inner-user" />
         </div>
+
         <div className="navbar-extension">
-          {/* Contenu optionnel de l'extension, si nécessaire */}
+          {/* Optional extension content if necessary */}
         </div>
       </nav>
-
+    </div>
   );
 }
 
