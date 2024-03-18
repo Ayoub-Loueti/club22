a2b4ec1bec9bad703bdc4eb824e7be221572eed9
Raniabenali1 <benalirania855@gmail.com>
2024-03-18 14:16:35 +0100

    prof

--- backend/routes/UtilisateurRoute.js
+++ backend/routes/UtilisateurRoute.js
@@ -5,7 +5,10 @@ const authenticate = require('../middleware/authenticate.js');
 const optionalAuthenticate = require('../middleware/optionalAuthenticate.js');
 
 router.post('/signup', utilisateurController.signup);
-router.get('/activateAccount/:userId/:token',utilisateurController.activateAccount);
+router.get(
+  '/activateAccount/:userId/:token',
+  utilisateurController.activateAccount
+);
 router.post('/resendEmail', utilisateurController.resendActivationEmail);
 
 router.post('/login', utilisateurController.login);
@@ -15,25 +18,36 @@ router.get('/auth/google/callback', utilisateurController.googleAuthCallback);
 router.get('/auth/logout', utilisateurController.logout);
 
 router.put('/updateCompte', authenticate, utilisateurController.updateUser);
-router.patch('/updateNameSurnameGenre', authenticate, utilisateurController.updateNameSurnameGenre);
+router.patch(
+  '/updateNameSurnameGenre',
+  authenticate,
+  utilisateurController.updateNameSurnameGenre
+);
 router.get('/rechercher/:id', authenticate, utilisateurController.findUser);
 
 router.post('/forgot-password', utilisateurController.forgotPassword);
 router.post('/check-reset-token', utilisateurController.checkResetToken);
 router.post('/reset-password/:token', utilisateurController.resetPassword);
-router.post('/resend-forgot-password-email/:email', utilisateurController.resendForgotPasswordEmail);
+router.post(
+  '/resend-forgot-password-email/:email',
+  utilisateurController.resendForgotPasswordEmail
+);
 router.get('/profil/:id', authenticate, utilisateurController.getUserProfile);
-router.get('/randomUsers', optionalAuthenticate, utilisateurController.getRandomUsers);
+router.get(
+  '/randomUsers',
+  optionalAuthenticate,
+  utilisateurController.getRandomUsers
+);
 
 const upload = require('../middleware/multerConfig');
 
 // Update user profile picture route
 router.post('/updateProfilePicture', authenticate, (req, res) => {
   upload(req, res, (err) => {
-    if(err){
+    if (err) {
       res.status(400).json({ error: err });
     } else {
-      if(req.file == undefined){
+      if (req.file == undefined) {
         res.status(400).json({ error: 'No file selected' });
       } else {
         // Here, you can now save the file path to the user's record in the database
@@ -44,5 +58,9 @@ router.post('/updateProfilePicture', authenticate, (req, res) => {
     }
   });
 });
-
+router.delete(
+  '/profile-picture',
+  authenticate,
+  utilisateurController.deleteProfilePicture
+);
 module.exports = router;
